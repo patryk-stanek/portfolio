3310ea7c2933d6bbb779f61aade07c807bb3a0cb
Patryk Stanek <p.stanek3@gmail.com>
2019-10-16 16:57:50 +0200

    Base files created

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Skills extends React.Component {
+    render() {
+        return (
+            <div>
+                Skills loaded!
+            </div>
+        )
+    }
+}
