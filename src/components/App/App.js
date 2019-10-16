cff9d67db1139844ba8c1f3d2da7584f372f9771
Patryk Stanek <p.stanek3@gmail.com>
2019-10-16 16:58:28 +0200

    Base files created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.scss";
 
+//Importing components
+import { Header } from "../Header/Header";
+import { About } from "../About/About";
+import { Skills } from "../Skills/Skills";
+import { Numbers } from "../Numbers/Numbers";
+import { Projects } from "../Projects/Projects";
+import { Contact } from "../Contact/Contact";
+import { Footer } from "../Footer/Footer"
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +17,15 @@ function App() {
         <h1>Patryk Stanek</h1>
         <h2>Junior Front End Developer</h2>
       </header>
+      <div>
+        <Header />
+        <About />
+        <Skills />
+        <Numbers />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Projects extends React.Component {
+    render() {
+        return (
+            <div>
+                Projects loaded!
+            </div>
+        )
+    }
+}
