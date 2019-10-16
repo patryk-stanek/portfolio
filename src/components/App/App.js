0fed11c2c318f689bf559901eea07bf3c820b767
Patryk Stanek <p.stanek3@gmail.com>
2019-10-16 16:41:33 +0200

    Base folder structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Patryk Stanek</h1>
+        <h2>Junior Front End Developer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
