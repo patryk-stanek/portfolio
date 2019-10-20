111096f1bd3993b5b07ce16b84e27ca127393e1e
Patryk Stanek <p.stanek3@gmail.com>
2019-10-20 20:44:40 +0200

    Finished Footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,15 @@ import "./Footer.scss"
 export class Footer extends React.Component {
     render() {
         return (
-            <div className="footer">
-                Footer loaded!
-            </div>
+            <footer className="footer">
+                <div className="footer__background-image">
+                    <div className="footer__background-content">
+                        <div className="footer__container">
+                            <span className="footer__text">All rights reserved. Patryk Stanek, 2019.</span>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         )
     }
 }
