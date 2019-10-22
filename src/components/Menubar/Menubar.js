20f1e53006ad480db22893b43a574d83f91159d9
Patryk Stanek <p.stanek3@gmail.com>
2019-10-22 18:52:48 +0200

    Changed Menubar style to horizontal

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -3,14 +3,40 @@ import React from "react";
 import "./Menubar.scss";
 
 export class Menubar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            top: 0,
+            bgc: "transparent",
+            class: "menubar"
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.listenScrollEvent)
+    }
+
+    listenScrollEvent = e => {
+        if (window.scrollY > 70) {
+            this.setState({
+                class: "menubar menubar__extra"
+            })
+        } else {
+            this.setState({
+                class: "menubar"
+            })
+        }
+    }
+
     render() {
         return (
-            <div className="menubar">
+            <div className={this.state.class}>
                 <span className="menubar__logo">PS</span>
+                <span className="menubar__language">
+                    EN / PL
+                </span>
                 <div className="menubar__controls">
-                    <span className="menubar__language">
-                        EN / PL
-                    </span>
                     <nav className="menubar__navigation">
                         <input 
                             type="checkbox"
